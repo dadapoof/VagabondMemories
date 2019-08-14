fae2af3f2610c56271c34df7ffb384b54d77cfd3
David Massart <32443128+dadapoof@users.noreply.github.com>
2019-08-14 09:47:20 -0400

    Starting layout of map

---
+++ main.js
@@ -0,0 +1,174 @@
+
+/* The Map */
+var map;
+
+function initialize() {
+	var options = {
+		zoom: 2,
+		center: new google.maps.MapTypeId.ROADMAP
+	};
+
+map = new google.maps.Map(document.getElementById('map-canvas'), options);
+
+var styledMapType = new google.maps.StyledMapType(
+[
+  {elementType: 'geometry', stylers: [{color: '#ebe3cd'}]},
+  {elementType: 'labels.text.fill', stylers: [{color: '#523735'}]},
+  {elementType: 'labels.text.stroke', stylers: [{color: '#f5f1e6'}]},
+  {
+    featureType: 'administrative',
+    elementType: 'geometry.stroke',
+    stylers: [{color: '#c9b2a6'}]
+  },
+  {
+    featureType: 'administrative.land_parcel',
+    elementType: 'geometry.stroke',
+    stylers: [{color: '#dcd2be'}]
+  },
+  {
+    featureType: 'administrative.land_parcel',
+    elementType: 'labels.text.fill',
+    stylers: [{color: '#ae9e90'}]
+  },
+  {
+    featureType: 'landscape.natural',
+    elementType: 'geometry',
+    stylers: [{color: '#dfd2ae'}]
+  },
+  {
+    featureType: 'poi',
+    elementType: 'geometry',
+    stylers: [{color: '#dfd2ae'}]
+  },
+  {
+    featureType: 'poi',
+    elementType: 'labels.text.fill',
+    stylers: [{color: '#93817c'}]
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'geometry.fill',
+    stylers: [{color: '#a5b076'}]
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'labels.text.fill',
+    stylers: [{color: '#447530'}]
+  },
+  {
+    featureType: 'road',
+    elementType: 'geometry',
+    stylers: [{color: '#f5f1e6'}]
+  },
+  {
+    featureType: 'road.arterial',
+    elementType: 'geometry',
+    stylers: [{color: '#fdfcf8'}]
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry',
+    stylers: [{color: '#f8c967'}]
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry.stroke',
+    stylers: [{color: '#e9bc62'}]
+  },
+  {
+    featureType: 'road.highway.controlled_access',
+    elementType: 'geometry',
+    stylers: [{color: '#e98d58'}]
+  },
+  {
+    featureType: 'road.highway.controlled_access',
+    elementType: 'geometry.stroke',
+    stylers: [{color: '#db8555'}]
+  },
+  {
+    featureType: 'road.local',
+    elementType: 'labels.text.fill',
+    stylers: [{color: '#806b63'}]
+  },
+  {
+    featureType: 'transit.line',
+    elementType: 'geometry',
+    stylers: [{color: '#dfd2ae'}]
+  },
+  {
+    featureType: 'transit.line',
+    elementType: 'labels.text.fill',
+    stylers: [{color: '#8f7d77'}]
+  },
+  {
+    featureType: 'transit.line',
+    elementType: 'labels.text.stroke',
+    stylers: [{color: '#ebe3cd'}]
+  },
+  {
+    featureType: 'transit.station',
+    elementType: 'geometry',
+    stylers: [{color: '#dfd2ae'}]
+  },
+  {
+    featureType: 'water',
+    elementType: 'geometry.fill',
+    stylers: [{color: '#b9d3c2'}]
+  },
+  {
+    featureType: 'water',
+    elementType: 'labels.text.fill',
+    stylers: [{color: '#92998d'}]
+  }
+],
+{name: 'Styled Map'});
+
+
+map.setOptions({styles: styledMapType});
+
+    // Initialize JSONP request
+    var script = document.createElement('script');
+    var url = ['https://www.googleapis.com/fusiontables/v1/query?'];
+    url.push('sql=');
+    var query = 'SELECT name, kml_4326 FROM ' +
+        '1foc3xO9DyfSIF6ofvN0kp2bxSfSeKog5FbdWdQ';
+    var encodedQuery = encodeURIComponent(query);
+    url.push(encodedQuery);
+    url.push('&callback=drawMap');
+    url.push('&key=AIzaSyAm9yWCV7JPCTHCJut8whOjARd7pwROFDQ');
+    script.src = url.join('');
+    var body = document.getElementsByTagName('body')[0];
+    body.appendChild(script);
+  }
+
+function drawMap(data) {
+	var rows = data['rows'];
+	for (var i in rows) {
+		if (rows[i][0] != "Antarctica") {
+			var newCoords = [];
+			var geometries = rows[i][1]["geometries"];
+			if (geometries) {
+				for (var j in geometries) {
+					newCoords.push(constructNewCoordinates(geometries[j]));
+				}
+			} else {
+				newCoords = constructNewCoordinates(rows[i][1]['geometry']);
+			}
+
+			var country = new google.maps.Polygon({
+				paths: newCoords,
+				strokeColor: '#ff9900',
+				strokeOpacity: 1,
+				strokeWeight: 0.3,
+				fillColor: '#ffff66',
+				fillOpacity: 0,
+				name: rows[i][0]
+			});
+			google.maps.event.addListener(country, 'mouseover', function() {
+				this.setOptions({fillOpacity: 0.4});
+			});
+			google.maps.event.addListener(country, )
+		}
+	}
+}
+
